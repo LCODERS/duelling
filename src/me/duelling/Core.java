612695e1864b724fe8799a1278088aa9ce546e9d
DarkStarPvP <speedcraft@klitgaard.de>
2015-11-03 18:51:25 +0100

    Console sender

--- src/me/duelling/Core.java
+++ src/me/duelling/Core.java
@@ -1,5 +1,13 @@
 package me.duelling;
 
-public class Core {
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Core extends JavaPlugin implements Listener {
+	
+	public void onEnable() {
+		Bukkit.getConsoleSender().sendMessage("Duel plugin made by _ItzMarcus_ and Dempsey.");
+	}
 
 }
