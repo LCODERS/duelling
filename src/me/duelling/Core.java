66f9ff773ebb3886c268105fcbeb50aa55a6c7ea
DarkStarPvP <speedcraft@klitgaard.de>
2015-11-03 19:11:10 +0100

    static plugin
    Just made the Core class access from other classes

--- src/me/duelling/Core.java
+++ src/me/duelling/Core.java
@@ -2,9 +2,12 @@ package me.duelling;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Core extends JavaPlugin implements Listener {
+	
+	private static Plugin plugin;
 	
 	public void onEnable() {
 		Bukkit.getConsoleSender().sendMessage("Duel plugin made by _ItzMarcus_ and Dempsey.");
