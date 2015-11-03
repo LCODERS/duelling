48bcebee56db78463ec3577055f1cf109c336610
DarkStarPvP <speedcraft@klitgaard.de>
2015-11-03 19:36:20 +0100

    Join message

--- src/me/duelling/listeners/JoinMessage.java
+++ src/me/duelling/listeners/JoinMessage.java
@@ -1,5 +1,26 @@
 package me.duelling.listeners;
 
-public class JoinMessage {
+import java.util.List;
+
+import me.duelling.Core;
+import net.md_5.bungee.api.ChatColor;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class JoinMessage implements Listener {
+	
+	Core plugin = new Core();
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e) {
+		Player p = e.getPlayer();
+		List<String> joinmessage = plugin.getConfig().getStringList("join-message");
+		for(String msg : joinmessage) {
+			p.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+		}
+	}
 
 }
