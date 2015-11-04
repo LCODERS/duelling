de584fc79124537d3e0828d39bea3b862234dd60
DarkStarPvP <speedcraft@klitgaard.de>
2015-11-04 18:24:01 +0100

    Core class

--- src/me/duelling/Core.java
+++ src/me/duelling/Core.java
@@ -1,12 +1,17 @@
 package me.duelling;
 
+import gq.dempsey.util.Arena;
+
 import java.io.IOException;
 
-import gq.dempsey.util.Arena;
+import me.duelling.listeners.CreateProfile;
+import me.duelling.listeners.JoinMessage;
+import me.duelling.utils.Profile;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Core extends JavaPlugin implements Listener {
@@ -20,6 +25,10 @@ public class Core extends JavaPlugin implements Listener {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		PluginManager pm = Bukkit.getServer().getPluginManager();
+		pm.registerEvents(new Profile(), this);
+		pm.registerEvents(new CreateProfile(), this);
+		pm.registerEvents(new JoinMessage(), this);
 		Bukkit.getConsoleSender().sendMessage("Duel plugin made by _ItzMarcus_ and Dempsey.");
 	}
 
